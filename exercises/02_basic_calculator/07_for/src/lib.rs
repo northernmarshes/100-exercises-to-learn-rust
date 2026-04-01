a3e1d5e71d8b58870ffbc99fd3ab4f89d85f16b1
northernmarshes <northernmarshes@gmail.com>
2026-04-02 00:11:38 +0200

    feat(02_basic_calculator/07_for): done

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,12 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+
+    let mut output: u32 = 1;
+
+    for i in 1..=n {
+        output = i * output;
+    }
+    output
 }
 
 #[cfg(test)]
